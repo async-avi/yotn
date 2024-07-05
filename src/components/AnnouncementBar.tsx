c12f88003cccdf67911d34bc5db5f809e8ace62c
Abhinav <abhinav.contact22@gmail.com>
2024-07-05 22:37:21 +0530

    Demo Announcement Bar

---
+++ src/components/AnnouncementBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AnnouncementBar() {
+  return (
+    <div className="h-10 w-screen bg-black flex justify-center items-center">
+      <h1 className="text-white text-xs">OFFER : BUY 2 GET 1 FREE AT 2999/-</h1>
+    </div>
+  );
+}
+
+export default AnnouncementBar;
