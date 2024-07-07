7aad18e5bfe26b3974fa1cae82af69822886b0e9
Abhinav <abhinav.contact22@gmail.com>
2024-07-07 23:37:43 +0530

    Fixed cors

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,34 @@ const nextConfig = {
         source: "/api/adminAuth/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              "https://1f290e8f-6615-48a5-ba20-eef28a350f1e-00-1iig1kblkzvbq.sisko.replit.dev",
+          },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            value: "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              "https://1f290e8f-6615-48a5-ba20-eef28a350f1e-00-1iig1kblkzvbq.sisko.replit.dev",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS",
           },
           {
             key: "Access-Control-Allow-Headers",
