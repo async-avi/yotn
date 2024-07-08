e22453f43390da306d21f189f5d22b2af14d81b1
Abhinav <abhinav.contact22@gmail.com>
2024-07-08 13:54:19 +0530

    CRUD done on declarations

---
+++ src/app/api/config/declarations/[id]/route.ts
@@ -0,0 +1,64 @@
+import errorHandler from "@/handlers/errorHandler";
+import { NextPageContext } from "next";
+import { NextResponse, type NextRequest } from "next/server";
+import prisma from "../../../../../../db";
+import asyncHandler from "@/handlers/asyncHandler";
+
+export async function PUT(req: NextRequest, context: any) {
+  const { params } = context;
+  const query: any = req.nextUrl.searchParams.get("f");
+  const requestBody = await req.json();
+  const cookie = req.cookies.get("adminAuth");
+  try {
+    if (!cookie) {
+      const response = await errorHandler(403, "Unauthorized");
+      return NextResponse.json(response);
+    }
+
+    const item = await prisma.declarations.findFirst({
+      where: {
+        id: params.id,
+      },
+    });
+
+    if (!item) {
+      const response = await errorHandler(404, "Item not found");
+      return NextResponse.json(response);
+    }
+
+    await prisma.declarations.update({
+      where: {
+        id: params.id,
+      },
+      data: {
+        [query]: requestBody.content,
+      },
+    });
+    const response = await asyncHandler(200, "Done", null);
+    return NextResponse.json(response);
+  } catch (error: any) {
+    const response = await errorHandler(500, error.message);
+    return NextResponse.json(response);
+  }
+}
+
+export async function DELETE(req: NextRequest, context: any) {
+  const { params } = context;
+  const cookie = req.cookies.get("adminAuth");
+  try {
+    if (!cookie) {
+      const response = await errorHandler(403, "Unauthorized");
+      return NextResponse.json(response);
+    }
+    await prisma.declarations.delete({
+      where: {
+        id: params.id,
+      },
+    });
+    const response = await asyncHandler(200, "Deleted", null);
+    return NextResponse.json(response);
+  } catch (error: any) {
+    const response = await errorHandler(500, error.message);
+    return NextResponse.json(response);
+  }
+}
