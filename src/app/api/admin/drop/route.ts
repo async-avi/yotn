750dbedda6ea5170e44084f71ff2cf6068e1ddda
Abhinav <abhinav.contact22@gmail.com>
2024-07-16 19:48:44 +0530

    CRUD Drop => admin

---
+++ src/app/api/admin/drop/route.ts
@@ -0,0 +1,49 @@
+import errorHandler from "@/handlers/errorHandler";
+import { type NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../../db";
+import asyncHandler from "@/handlers/asyncHandler";
+
+export async function GET(req: NextRequest) {
+  const auth = req.cookies.get("auth");
+  try {
+    if (!auth) {
+      const response = await errorHandler(403, "Unauthorized");
+      return NextResponse.json(response);
+    }
+    const drop = await prisma.drop.findMany({});
+    const response = await asyncHandler(202, "OK", drop);
+    return NextResponse.json(response);
+  } catch (error: any) {
+    const response = await errorHandler(500, `${error.message}`);
+    return NextResponse.json(response);
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const auth = req.cookies.get("auth");
+  const requestBody = await req.json();
+  const { name, imageUrl, createdBy, description } = requestBody;
+  try {
+    if (!auth) {
+      const response = await errorHandler(403, "Unauthorized");
+      return NextResponse.json(response);
+    }
+    const newDrop = await prisma.drop.create({
+      data: {
+        name,
+        description,
+        imageUrl,
+        createdBy,
+      },
+    });
+    const response = await asyncHandler(
+      201,
+      "Drop created successfully",
+      newDrop.id
+    );
+    return NextResponse.json(response);
+  } catch (error: any) {
+    const response = await errorHandler(500, `${error.message}`);
+    return NextResponse.json(response);
+  }
+}
