32907c4ab61bd50b79422972a888d6a25caba18d
Abhinav <abhinav.contact22@gmail.com>
2024-07-16 19:39:22 +0530

    CRUD products => Admin side

---
+++ src/app/api/admin/products/[id]/route.ts
@@ -0,0 +1,59 @@
+import errorHandler from "@/handlers/errorHandler";
+import { type NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../../../db";
+import asyncHandler from "@/handlers/asyncHandler";
+
+export async function PATCH(req: NextRequest, context: any) {
+  const auth = req.cookies.get("adminAuth");
+  const { params } = context;
+  const query: any = req.nextUrl.searchParams.get("q");
+  const set = req.nextUrl.searchParams.get("s");
+  try {
+    if (!auth) {
+      const response = await errorHandler(403, "Unauthorized");
+      return NextResponse.json(response);
+    }
+    const updated = await prisma.item.update({
+      where: {
+        id: params.id,
+      },
+      data: {
+        [query]: set,
+      },
+    });
+
+    const response = await asyncHandler(
+      200,
+      "updated",
+      `${updated.id} : ${query} updated to ${set}`
+    );
+
+    return NextResponse.json(response);
+  } catch (error: any) {
+    const response = await errorHandler(500, `${error.message}`);
+    return NextResponse.json(response);
+  }
+}
+
+export async function DELETE(req: NextRequest, context: any) {
+  const auth = req.cookies.get("adminAuth");
+  const { params } = context;
+
+  try {
+    if (!auth) {
+      const response = await errorHandler(403, "Unauthorized");
+      return NextResponse.json(response);
+    }
+    await prisma.item.delete({
+      where: {
+        id: params.id,
+      },
+    });
+
+    const response = await asyncHandler(200, `${params.id} is deleted`, null);
+    return NextResponse.json(response);
+  } catch (error: any) {
+    const response = await errorHandler(500, `${error.message}`);
+    return NextResponse.json(response);
+  }
+}
---
+++ src/app/api/admin/products/route.ts
@@ -0,0 +1,78 @@
+import errorHandler from "@/handlers/errorHandler";
+import { NextResponse, type NextRequest } from "next/server";
+import prisma from "../../../../../db";
+import asyncHandler from "@/handlers/asyncHandler";
+
+export async function GET(req: NextRequest) {
+  const auth = req.cookies.get("auth");
+  try {
+    if (!auth) {
+      const response = await errorHandler(403, "Unauthorized");
+      return NextResponse.json(response);
+    }
+
+    const products = await prisma.item.findMany({});
+    let response = await asyncHandler(202, "OK", products);
+    return NextResponse.json(response);
+  } catch (error: any) {
+    console.error(error);
+    let response = await errorHandler(500, `${error.message}`);
+    return NextResponse.json(response);
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const auth = req.cookies.get("adminAuth");
+  const requestBody = await req.json();
+  const {
+    name,
+    description,
+    actualPrice,
+    hikedPrice,
+    images,
+    category,
+    sizes,
+    dropName,
+  } = requestBody;
+
+  try {
+    if (!auth) {
+      const response = await errorHandler(403, "Unauthorized");
+      return NextResponse.json(response);
+    }
+
+    const dropAsked = await prisma.drop.findFirst({
+      where: {
+        name: dropName,
+      },
+    });
+
+    const newProduct = await prisma.item.create({
+      data: {
+        name,
+        description,
+        actualPrice,
+        hikedPrice,
+        images,
+        category,
+        sizes,
+        drop: {
+          connect: {
+            id: dropAsked?.id,
+          },
+        },
+      },
+    });
+
+    const response = await asyncHandler(
+      201,
+      "Product created successfully",
+      newProduct.id
+    );
+
+    return NextResponse.json(response);
+  } catch (error: any) {
+    const response = await errorHandler(500, `${error.message}`);
+    return NextResponse.json(response);
+  }
+}
