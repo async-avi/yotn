b4880574ae802e13e3d904917b7a6b62bc91b5ff
Abhinav <abhinav.contact22@gmail.com>
2024-07-20 20:49:35 +0530

    CORS update

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/admin/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
