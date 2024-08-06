78fd5beec0c443a6a403659bd5f98f4ff6999161
Abhinav <abhinav.contact22@gmail.com>
2024-08-06 23:50:13 +0530

    front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import PhoneV4 from "../../public/images/Phone v4.png";
+import Image from "next/image";
 
 function page() {
-  return <div></div>;
+  return (
+    <>
+      <main className="p-6 bg-white w-screen h-lvh overflow-hidden md:p-12">
+        <div className=" lg:bg-red-400">
+          <h1 className="text-4xl font-extrabold uppercase md:text-8xl">
+            With elegance
+          </h1>
+          <h1 className="text-4xl font-extrabold uppercase md:text-8xl">
+            you own the night
+          </h1>
+          <div></div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default page;
